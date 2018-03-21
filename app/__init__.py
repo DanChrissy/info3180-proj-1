054786c899a881508a273cbc81f448e4b9b8cae7
Danielle Scott <danielle.scott02@mymona.uwi.edu>
2018-03-21 23:49:35 +0000

    Part B-Heroku

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Sup3r$3cretkey'
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://proj1:infoProject1@localhost/proj1"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://qmamvhsfyzobuc:198505cbbea1ab13419fd8d8f40fc17f79db0e7b2de963b7c96c1f099c8a3f1a@ec2-54-204-44-140.compute-1.amazonaws.com:5432/d88kvh89dd68fq'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 
 db = SQLAlchemy(app)
